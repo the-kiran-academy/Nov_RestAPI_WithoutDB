cb1bd83c95da6c45f0875f73d960a52575e56936
ram-chadar <javaprojectwork2715@gmail.com>
2023-02-13 20:00:57 +0530

    task1bug

--- src/main/java/com/jbk/daoIMPL/ProductDaoIMPL.java
+++ src/main/java/com/jbk/daoIMPL/ProductDaoIMPL.java
@@ -2,6 +2,8 @@ package com.jbk.daoIMPL;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.IntStream;
 
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
@@ -13,6 +15,13 @@ import com.jbk.model.Product;
 public class ProductDaoIMPL implements ProductDao {
 
 	List<Product> list = new ArrayList<>();
+
+	public ProductDaoIMPL() {
+		list.add(new Product(1, "pen", 1, 1, 10, 10));
+		list.add(new Product(2, "pencil", 1, 1, 10, 10));
+		list.add(new Product(3, "book", 1, 1, 10, 100));
+
+	}
 
 	@Override
 	public boolean saveProduct(Product product) {
@@ -29,7 +38,24 @@ public class ProductDaoIMPL implements ProductDao {
 
 	@Override
 	public boolean deleteProductById(long id) {
-		return false;
+
+//		IntStream.range(0, list.size())
+//        .filter(i -> list.get(i).getProductId()==id)
+//        .boxed()
+//        .findFirst()
+//        .map(i -> list.remove((int) i));
+//		return true;
+
+		boolean isDeleted = false;
+		try {
+			Product product = list.stream().filter(listProduct -> listProduct.getProductId() == id).findFirst().get();
+			list.remove(product);
+			isDeleted = true;
+		} catch (NoSuchElementException e) {
+			isDeleted = false;
+		}
+		return isDeleted;
+
 	}
 
 }
